b03fbc2c20f4db5016acc963f58d4e3170f5072e
Meta-V <142105300+Meta-V@users.noreply.github.com>
2023-09-24 10:31:12 +0800

    Add files via upload

---
+++ aliyun_uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget "http://update2.aegis.aliyun.com/download/uninstall.sh" && chmod +x uninstall.sh && ./uninstall.sh
+/usr/local/share/assist-daemon/assist_daemon --stop
+/usr/local/share/assist-daemon/assist_daemon --delete
+rm -rf /usr/local/share/assist-daemon
+dpkg -l | grep aliyun_assist | awk '{print $2}' | xargs dpkg -P
